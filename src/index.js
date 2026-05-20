68f11accb7741ee55ada57016be487024f4be363
cynjean <cynthia.2056@yahoo.com>
2026-05-20 15:57:02 -0500

    Lab 3 completed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //
+  //Select Form
+  const form = document.querySelector("#create-task-form") 
+  // Add an event listener to the form and use preventDefault
+  function handleSubmit(event) {
+    event.preventDefault()
+    //Handle form data 
+    const newTask = event.target["new-task-description"].value
+    buildToDo(newTask)
+  }
+  //Create function and call it
+  function buildToDo(newTask) {
+    //create a new li element
+    const taskList = document.querySelector("#tasks")
+    const li = document.createElement("li")
+    li.textContent = newTask
+    taskList.append(li)
+
+    console.log(newTask)
+  }
+
+  form.addEventListener("submit", (event) => {
+    handleSubmit(event)
+  })
 });
